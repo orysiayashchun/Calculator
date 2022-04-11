aafb775f17ed974bcd35159d08228a667dbc50a4
orysiayashchun <54995877+orysiayashchun@users.noreply.github.com>
2022-04-11 10:55:47 +0300

    Added CheckCuttrncy

--- AnalaizerClass/AnalaizerClass.cs
+++ AnalaizerClass/AnalaizerClass.cs
@@ -17,7 +17,89 @@ namespace AnalaizerClass
             lastError = "";
         }
 
-       
+        private void CheckCurrency()
+        {
+            lengthCheck();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                wrongSymbCheck(expression[i], i + 1);//перевіряємо чи коректрий символ
+            }
+
+            symbIsLetter();//чи коректна операція
+            bracketsCheck();//перевірка дужок
+        }
+
+        //перевірка дужок
+        private void bracketsCheck()
+        {
+
+            int openedBracketsCount = 0;
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char symb = expression[i];
+
+                if (symb == '(')
+                {
+                    openedBracketsCount++;
+                }
+                else if (symb == ')')
+                {
+                    openedBracketsCount--;
+                }
+
+                if (openedBracketsCount < 0)
+                {
+                    lastError = "ERROE 01 : at " + (i + 1) + "  wrong brackets structure";
+                    throw new Exception(lastError);
+                }
+                else if (openedBracketsCount > 3)//Максимальна глибина вкладеності структури в дужках – 3.
+                {
+                    lastError = "ERROR 01 : at " + (i + 1) + " : wrong brackets structure";
+                    throw new Exception(lastError);
+                }
+            }
+
+            if (openedBracketsCount != 0)
+            {
+                lastError = "ERROR 03: Incorrect syntax";
+                throw new Exception(lastError);
+            }
+        }
+        //перевірка чи правильно введений символ
+        private void symbIsLetter()
+        {
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if ((expression[i] == 'm') &&
+                    (i < expression.Length - 2) &&
+                    (expression[i + 1] == 'o') &&
+                    (expression[i + 2] == 'd'))
+                {
+                    i += 2;
+                    continue;
+                }
+
+                if (expression[i] == 'm' ||
+                    expression[i] == 'p' ||
+                    Char.IsDigit(expression[i]) ||
+                    Char.IsWhiteSpace(expression[i]) ||
+                    expression[i] == '/' ||
+                    expression[i] == '*' ||
+                    expression[i] == '-' ||
+                    expression[i] == '+' ||
+                    expression[i] == '(' ||
+                    expression[i] == ')')
+                {
+                    continue;
+                }
+
+                lastError = "Error 02 at " + (i + 1) + " : Unknown operator";
+                throw new Exception(lastError);
+            }
+        }
+
 
         private bool isAnumb(string line)
         {
